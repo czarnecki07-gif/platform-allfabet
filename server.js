6ffcb1ab173d21aa67bf1dc599c4f76e21aca795
Grzegorz Czarnecki <czarnecki07@gmail.com>
2026-04-18 15:18:24 +0200

    serve lesson media files

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(cors({
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/custom_media', express.static(path.join(process.cwd(), 'custom_media')));
+app.use('/generated_audio', express.static(path.join(process.cwd(), 'generated_audio')));
+app.use('/generated_media', express.static(path.join(process.cwd(), 'generated_media')));
+
 app.use(session({
   secret: process.env.SESSION_SECRET || 'supersecret123',
   resave: false,
